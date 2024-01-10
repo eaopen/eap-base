688534eccc9bb8833731b9664aa85b672301a4ca
dev <dev@dev.com>
2024-01-10 13:07:43 +0800

    update PageResult

--- eap-common-libs/eap-common/src/main/java/org/openea/eap/framework/common/pojo/PageResult.java
+++ eap-common-libs/eap-common/src/main/java/org/openea/eap/framework/common/pojo/PageResult.java
@@ -17,12 +17,30 @@ public final class PageResult<T> implements Serializable {
     @Schema(description = "总量", requiredMode = Schema.RequiredMode.REQUIRED)
     private Long total;
 
+    @Schema(description = "分页", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private PaginationVO pagination;
+
     public PageResult() {
     }
 
     public PageResult(List<T> list, Long total) {
         this.list = list;
         this.total = total;
+        // 针对返回全部数据进行优化
+        if(this.pagination!=null){
+            this.pagination.total = this.total;
+            if(this.list!=null && this.list.size()>this.pagination.pageSize){
+                this.pagination.pageSize = (list.size()+1)%10*10;
+            }
+        }
+    }
+
+    public PageResult(List<T> list, PaginationVO pagination){
+        this.list = list;
+        this.pagination = pagination;
+        if(this.pagination!=null && this.total==null){
+            this.total = this.pagination.total;
+        }
     }
 
     public PageResult(Long total) {
